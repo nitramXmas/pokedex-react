0d5f88a9ee942b9f1f2a8c631c42501abc04916a
nitramXmas <24914535+nitramXmas@users.noreply.github.com>
2023-03-28 12:00:21 +0200

    react basics 03 done

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,19 +1,49 @@
 import React from 'react';
 
 const PokemonCard = () => {
-  return (
-    <div>
-      <figure>
-        <img 
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" 
-          alt="a wild bulbasaur appear" 
-        />
-        <figcaption>
-          bulbasaur
-        </figcaption>
-      </figure>
-    </div>
-  );
-};
+ 
+const pokemon = pokemonList[0]
+ 
+const hasImage = (pokemon) => {
+  if("imgSrc" in pokemon) {
+    return (
+          <div>
+            <figure>
+              <img 
+                src={pokemon.imgSrc}
+                alt={pokemon.name} 
+              />
+              <figcaption>
+                {pokemon.name}
+              </figcaption>
+            </figure>
+          </div>
+        )
+  } else {
+    return (
+          <div>
+            <figure>
+              <p>???</p>
+              <figcaption>
+                {pokemon.name}
+              </figcaption>
+            </figure>
+          </div>
+    )
+  }
+}
+return hasImage(pokemon)
+}
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 export default PokemonCard;
